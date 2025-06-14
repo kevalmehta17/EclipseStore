0507a9abd3b0f134fffac90fee1af1feac63d16b
Keval <kevalmehta885@gmail.com>
2025-06-14 16:20:45 +0530

    feat: add user model schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [6, "Password must be at least 6 characters long"],
+    },
+    cartItems: [
+        {
+            quantity: {
+                type: Number,
+                default: 0,
+            },
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+            }
+        }
+    ],
+    role: {
+        type: String,
+        enum: ["user", "admin"], //enum means the value must be one of these
+        default: "user",
+    }
+}, { timeStamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
