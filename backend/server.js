5697d15f310917cbdd81d835d3becc41920bfedd
Keval <kevalmehta885@gmail.com>
2025-06-14 16:08:06 +0530

    refactor: updated

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 6001;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 })
