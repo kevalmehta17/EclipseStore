f4eff826b0a609e899a1bb43c9f28953d8f77368
Keval <kevalmehta885@gmail.com>
2025-06-18 11:15:13 +0530

    feat: created all Product route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -13,7 +13,7 @@ router.post("/logout", logout)
 
 router.post("/refresh-token", refreshToken);
 
-router.get("/profile", getProfile);
+// router.get("/profile", getProfile);
 
 
 export default router;
